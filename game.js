103386394e6d3188dc65b358e7d8923ffe079bdd
jwang247 <wangjunfengapply@163.com>
2023-03-14 23:48:53 -0400

    Update game.js

--- game.js
+++ game.js
@@ -359,7 +359,6 @@
 		  let takeNum = 1;
 		  arrNum[3] = 6;
 		  document.getElementById("action").innerHTML = '小王从第四区域中拿了' + takeNum +'个,棋子由7个变为6个';
-		  playerCount++;
 		  pick(area, takeNum);
 		  }
 		  , 3000);	  
